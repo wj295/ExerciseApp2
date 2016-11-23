ab679c509547dc42f1064fae51dff361d7e2c070
wj295 <zin295@yahoo.com.tw>
2016-11-24 01:04:16 +0800

    test

--- app/src/main/java/com/wj/exerciseapp2/MainActivity.java
+++ app/src/main/java/com/wj/exerciseapp2/MainActivity.java
@@ -2,6 +2,10 @@ package com.wj.exerciseapp2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View view) {
+
+        //button = (Button) findViewById(R.id.button);
+        TextView textView = (TextView) findViewById(R.id.textView2);
+
+        Toast toast = Toast.makeText(this, R.string.buttonOnClickMessage + textView.getText().toString(), Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
